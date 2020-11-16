bb950d738b47f63ede5d6180412f3ee37c28ac88
Jake Ritter <jake.ritter@sequoia.com>
2020-11-16 13:58:17 -0800

    Update script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,17 +1,232 @@
 'use strict';
 
-function logger(){
-  console.log('My name is Jake')
+/*
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+
+
+///////////////////////////////////////
+// Functions
+function logger() {
+  console.log('My name is Jonas');
 }
 
+// calling / running / invoking function
 logger();
 logger();
 logger();
 
-function fruitProcessor(apples, oranges){
-  console.log(apples,oranges);
-  const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
   return juice;
 }
 
-console.log(`I made a ${fruitProcessor(2,5)}`)
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+
+///////////////////////////////////////
+// Function Declarations vs. Expressions
+
+// Function declaration
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
+}
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+///////////////////////////////////////
+// Arrow functions
+
+const calcAge3 = birthYeah => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas')); console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+///////////////////////////////////////
+// Functions Calling Other Functions
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+///////////////////////////////////////
+// Reviewing Functions
+const calcAge = function (birthYeah) {
+  return 2037 - birthYeah;
+}
+
+const yearsUntilRetirement = function (birthYeah, firstName) {
+  const age = calcAge(birthYeah);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+
+*/
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 😉
+
+GOOD LUCK 😀
+*/
+const team1 = 'Dolphins';
+const team2 = 'Koalas';
+
+const dolphinsScore1 = [44, 23, 71];
+const koalasScore1 = [65, 54, 49];
+
+const dolphinsScore2 = [85, 54, 41];
+const koalasScore2 = [23, 34, 27];
+
+const calcAverage = (score1, score2, score3) => ((score1 + score2 + score3)/3);
+
+function checkWinner(team1, avgScore1, team2, avgScore2) {
+  if (avgScore1 > avgScore2) {
+    return(team1 + " is the winner! " + avgScore1 + " vs " + avgScore2)
+  }
+  else if (avgScore1 < avgScore2) {
+    return(team2 + " is the winner! " + avgScore1 + " vs " + avgScore2)
+  }
+  else {
+    return("It's a tie!")
+  }
+}
+
+console.log("For game 1, the " + team1 + " scored an average of: " + calcAverage(dolphinsScore1[0],dolphinsScore1[1],dolphinsScore1[2]));
+console.log("For game 1, the " + team2 + " scored an average of: " + calcAverage(koalasScore1[0],koalasScore1[1],koalasScore1[2]));
+console.log("For game 1, " + checkWinner(team1, calcAverage(dolphinsScore1[0],dolphinsScore1[1],dolphinsScore1[2]), team2, calcAverage(koalasScore1[0],koalasScore1[1],koalasScore1[2])));
+
+console.log("For game 2, the " + team1 + " scored an average of: " + calcAverage(dolphinsScore2[0],dolphinsScore2[1],dolphinsScore2[2]));
+console.log("For game 2, the " + team2 + " scored an average of: " + calcAverage(koalasScore2[0],koalasScore2[1],koalasScore2[2]));
+console.log("For game 2, " + checkWinner(team1, calcAverage(dolphinsScore2[0],dolphinsScore2[1],dolphinsScore2[2]), team2, calcAverage(koalasScore2[0],koalasScore2[1],koalasScore2[2])));
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+
+
+///////////////////////////////////////
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+
+class Person {
+        constructor(name, height, weight) {
+        name = self.name
+        height = self.height
+        weight = self.weight
+        }
+        calcBMI(height, weight) {
+          weight / (height**2)
+        }
+}
+
+const mark = new Person('Mark', 1.69, 78)
+const john = new Person('John', 1.95, 92)
+
+console.log(mark.name + " weighs " + mark.weight + "kgs, and is " + mark.height + "m's tall.")
+console.log(john.name + " weighs " + john.weight + "kgs, and is " + john.height + "m's tall.")
