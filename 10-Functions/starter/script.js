a728b12406987d293c4fac28961101b6e3c58eba
Jake Ritter <60155785+jakerittersequoia@users.noreply.github.com>
2020-12-28 10:58:35 -0800

    js coursework

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+/*const oneWord = function(str) {
+  return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+}
+
+//Higher Order function (takes a function as an argument)
+const transformer = function(str, fn) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best', oneWord);
+*/
+const greet = function(greeting) {
+  
+}
