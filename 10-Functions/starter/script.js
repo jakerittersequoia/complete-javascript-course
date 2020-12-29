620bcded702b5ad01b77958e9bc05bc13461041d
Jake Ritter <60155785+jakerittersequoia@users.noreply.github.com>
2020-12-29 13:27:09 -0800

    more coursework

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -17,6 +17,44 @@ const transformer = function(str, fn) {
 
 transformer('JavaScript is the best', oneWord);
 */
+/*
 const greet = function(greeting) {
-  
+  return function(name) {
+    console.log(`${greeting} ${name}`);
+  }
 }
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Jake');
+
+greet('Hello')('Jake');
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`)
+
+greetArr('Hello')('Jake');
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on the ${this.airline} flight ${this.iataCode}-${flightNum}`);
+    this.bookings.push({flight: `${this.iataCode}-${flightNum}`, name})
+  }
+}
+
+lufthansa.book(239, 'Jake Ritter');
+lufthansa.book(240, 'Kelly Ritter');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book();
+
+book.call(eurowings, 23, 'Robin Williams');
+console.log(eurowings)
