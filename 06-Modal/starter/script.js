84809cef8f5b7f568e2786d8c39f00e76465fb4a
Jake Ritter <60155785+jakerittersequoia@users.noreply.github.com>
2021-01-08 16:25:58 -0800

    learn about modals

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function(){
+                                modal.classList.add('hidden');
+                                overlay.classList.add('hidden');
+                              };
+
+const openModal = function(){
+                              console.log('Button clicked');
+                              modal.classList.remove('hidden');
+                              overlay.classList.remove('hidden');
+                            };
+
+btnCloseModal.addEventListener('click',closeModal);
+
+overlay.addEventListener('click',closeModal);
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+                                                  console.log(btnsShowModal[i].textContent)
+                                                  btnsShowModal[i].addEventListener('click',openModal);
+                                                };
+
+document.addEventListener('keydown', function(e){
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')){
+      closeModal();
+  };
+});
