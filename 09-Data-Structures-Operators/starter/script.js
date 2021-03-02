8629a83487a6103cafc144b59635783f0e38d86f
Jake Ritter <60155785+jakerittersequoia@users.noreply.github.com>
2021-03-01 17:10:29 -0800

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -22,3 +22,23 @@ const restaurant = {
     },
   },
 };
+
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr;
+console.log(x,y,z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+//const temp = main;
+//main = secondary;
+//secondary = main;
+//console.log(main,secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main,secondary);
